225046ff36e7107dd3703d3e32351356338ad101
Spencer Nauman <snauman817@gmail.com>
2019-03-20 14:59:48 -0400

    Additional routes and tests created - creates hello/ route which returns a page with Hello World! or Hello Name! - creates tests for the hello/ route which test no query param and a query param of a name - creates number/<n>/ route which displays Number: n where n is given - creates tests for number/<n>/ that checks if the correct response is returned and that only ints can be given

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
 
+# this is our factory
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
@@ -14,8 +15,25 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+    # The route place to respond to a certain request (this one accepts GET, HEAD, OPTIONS)
     @app.route('/')
     def index():
-        return 'Hello World!'
+        return 'This is a flask-boilerplate project, not to be used in production.'
+
+    @app.route('/hello')
+    def hello():
+        print(request.args)
+        print(type(request.args))
+
+        # # loops through all query parameters, and grabs the key and value of each query parameter
+        # for key, value in request.args.items():
+        #     print(f"{key}: {value}")
+
+        name = request.args.get('name', 'World')
+        return f"Hello {name}!"
+    
+    @app.route('/number/<int:n>')
+    def number(n):
+        return f"Number: {n}"
 
     return app
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,24 @@ def test_config():
 
 def test_index(client):
     response = client.get('/')
+    assert response.data == b'This is a flask-boilerplate project, not to be used in production.'
+
+def test_hello(client):
+    # if the url has no query parameter, it should show Hello World!
+    # the get function doesn't error when the value wanted to be returned does not exist
+    response = client.get('/hello')
     assert response.data == b'Hello World!'
+
+    # if Jim is in the query parameter as name, it should return Hello Jim!
+    response = client.get('/hello?name=Jim')
+    assert response.data == b'Hello Jim!'
+
+def test_number(client):
+    response = client.get('/number/1')
+    assert response.data == b'Number: 1'
+
+    response = client.get('/number/2')
+    assert response.data == b'Number: 2'
+
+    response = client.get('/number/hi')
+    assert response.status_code == 404
